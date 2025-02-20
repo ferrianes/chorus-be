010a172820c18aef2aa2df8995c987dc239cb8de
Ferrian Septiawan <ferrian27@gmail.com>
2025-02-20 22:31:56 +0700

    feat: add CORS support and update dependencies in package.json

--- src/routes/api/v1.ts
+++ src/routes/api/v1.ts
@@ -1,9 +1,11 @@
 import { auth } from "@/controllers/v1/auth";
+import cors from "@elysiajs/cors";
 import Elysia from "elysia";
 
 const apiV1 = new Elysia()
   .group("/api/v1", (app) =>
     app
+      .use(cors())
       .use(auth)
   )
 
